22784f59a5cb22e0892894cb64aafad8b356e1c0
“adeade00” <adrian.levin@gmail.com>
2023-02-19 21:38:08 +0000

    minor

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+string testCharacter;
+
+testCharacter = Blocks.BlockCharacters._decimalToUnicode[10];
+
+Console.WriteLine(testCharacter);
+
+
+Console.WriteLine("ending");
